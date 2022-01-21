182e893e492876c13cdf2181974361a383bdf0a3
Damianp99 <damianperez200199@gmail.com>
2022-01-21 17:10:46 +0100

    snack2 done

--- js/snack2.js
+++ js/snack2.js
@@ -37,8 +37,15 @@ const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)
 for (let i = 0; i < teams.length; i++) {
     const team = teams[i];
     team.puntiFatti = getRandomNumber(1, 100)
-    team.falliSubiti = getRandomNumber(1, 100)
+    team.falliSubiti = getRandomNumber(1, 20)
 }
 
 console.log(teams)
 const newStats = [];
+
+for (let i = 0; i < teams.length; i++) {
+    const team = teams[i]
+    const { name, puntiFatti, falliSubiti } = team;
+    newStats.push(name, puntiFatti, falliSubiti)
+}
+console.log(newStats)
