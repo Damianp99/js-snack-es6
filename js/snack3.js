dbb649e2d913f2ec2a9f9473eefb7221545597a9
Damianp99 <damianperez200199@gmail.com>
2022-01-21 17:19:00 +0100

    starting new snacks

---
+++ js/snack3.js
@@ -0,0 +1,18 @@
+/*
+SNACK 3
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array visti oggi in classe.
+*/
+
+const images = [
+    { format: 'JPG', imgSize: 340 },
+    { format: 'PNG', imgSize: 182 },
+    { format: 'GIF', imgSize: 287 },
+    { format: 'JPG', imgSize: 451 },
+    { format: 'GIF', imgSize: 428 },
+    { format: 'JPG', imgSize: 199 },
+    { format: 'PNG', imgSize: 347 },
+]
+
+console.log(images);
